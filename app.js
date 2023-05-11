94b80e7370dd499ac6e712f54a0dadf8b6e140f5
Eman_devs <emansalahuddinnajmi@gamil.com>
2023-05-11 21:47:05 +0500

    initialzie project

---
+++ app.js
@@ -0,0 +1,47 @@
+// TASK NO 1[ function that check leap year]
+
+// var current = new Date( );
+// leapyear(current.getFullYear( ))
+// function leapyear(current){
+//     if(current %4==0){
+//         alert("current year"+ current +"is aleap year" )
+//     }
+//         else{
+//             alert("current year"+ current + "is not a leap year")
+//         }
+//     }
+
+// TASK NO 5[ Function that calculate your age]
+
+// var current = new Date( );
+// var DOB = new Date( prompt("Enter Your Age"))
+// calculateAge(current,DOB)
+
+
+// function calculateAge(current,DOB){
+//     var age = current - DOB
+//     alert("your age is"+ Math.floor(age/(1000*60*60*24*365)))
+// }
+
+// // TASK NO 2[Function that shows next 10 leap year]
+
+// var year = prompt("enter year to find next 10 leap years");
+// var list = nextleap(year);
+// document.write("next 10 leap years are: ");
+// document.write(list)
+
+// function nextleap(year){
+//     var i,leaplist = [];
+//     for(i=1;i<=10;i++){
+//         if(year%4==0){
+//             leaplist.push(year);
+//         }
+//         else{
+//             i--
+//         }
+//         year++
+//     }
+//     return leaplist;
+// }
+
+ 
